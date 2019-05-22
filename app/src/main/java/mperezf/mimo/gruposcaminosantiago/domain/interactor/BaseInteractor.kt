3304b250629c55a0d665add631f86ca9a0b5defb
Pérez Fernández, Miguel <mperezfe@minsait.com>
2019-05-22 17:00:58 +0200

    Create group data class

---
+++ app/src/main/java/mperezf/mimo/gruposcaminosantiago/domain/interactor/BaseInteractor.kt
@@ -0,0 +1,27 @@
+package mperezf.mimo.gruposcaminosantiago.domain.interactor
+
+import io.reactivex.Observable
+import io.reactivex.Scheduler
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.observers.DisposableObserver
+
+abstract class BaseInteractor<T, P>(var mMainThread: Scheduler, var mIOExecutor: Scheduler) {
+
+    private val mCompositeDisposable: CompositeDisposable = CompositeDisposable()
+
+    protected abstract fun result(params: P): Observable<T>
+
+    fun execute(observer: DisposableObserver<T>, params: P) {
+        val observable = result(params)
+            .subscribeOn(mIOExecutor)
+            .observeOn(mMainThread)
+        mCompositeDisposable.add(observable.subscribeWith(observer))
+    }
+
+
+    fun close() {
+        if (!mCompositeDisposable.isDisposed) {
+            mCompositeDisposable.dispose()
+        }
+    }
+}
---
+++ app/src/main/java/mperezf/mimo/gruposcaminosantiago/domain/model/Group.kt
@@ -0,0 +1,14 @@
+package mperezf.mimo.gruposcaminosantiago.domain.model
+
+data class Group(
+
+    val id: Int,
+    val photo: String,
+    val title: String,
+    val description: String,
+    val departureDate: Int,
+    val arrivalDate: Int,
+    val departurePlace: String,
+    val latitude: Double,
+    val longitude: Double
+)
