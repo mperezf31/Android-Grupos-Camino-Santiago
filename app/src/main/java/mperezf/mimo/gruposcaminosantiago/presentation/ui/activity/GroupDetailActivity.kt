31d64ca17ecf5d6cc4ea46594efb7a45df929dcc
mperezf <miguelperezfernandez31@gmail.com>
2019-06-08 13:23:41 +0200

    Add viewpager animation

--- app/src/main/java/mperezf/mimo/gruposcaminosantiago/presentation/ui/activity/GroupDetailActivity.kt
+++ app/src/main/java/mperezf/mimo/gruposcaminosantiago/presentation/ui/activity/GroupDetailActivity.kt
@@ -62,6 +62,7 @@ class GroupDetailActivity : AppCompatActivity(), GroupMemberListFragment.MemberF
 
         viewModel = ViewModelProviders.of(this).get(GroupDetailViewModel::class.java)
         nav_view_group_detail.setOnNavigationItemSelectedListener(onNavigationItemSelectedListener)
+        vp_group_detail.setPageTransformer(true, ZoomOutPageTransformer())
 
         addObservers()
 
@@ -151,5 +152,41 @@ class GroupDetailActivity : AppCompatActivity(), GroupMemberListFragment.MemberF
         }
     }
 
+}
 
+
+private const val MIN_SCALE = 0.85f
+private const val MIN_ALPHA = 0.5f
+
+class ZoomOutPageTransformer : ViewPager.PageTransformer {
+
+    override fun transformPage(view: View, position: Float) {
+        view.apply {
+            val pageWidth = width
+            val pageHeight = height
+            when {
+                position < -1 -> {
+                    alpha = 0f
+                }
+                position <= 1 -> {
+                    val scaleFactor = Math.max(MIN_SCALE, 1 - Math.abs(position))
+                    val vertMargin = pageHeight * (1 - scaleFactor) / 2
+                    val horzMargin = pageWidth * (1 - scaleFactor) / 2
+                    translationX = if (position < 0) {
+                        horzMargin - vertMargin / 2
+                    } else {
+                        horzMargin + vertMargin / 2
+                    }
+
+                    scaleX = scaleFactor
+                    scaleY = scaleFactor
+
+                    alpha = (MIN_ALPHA + (((scaleFactor - MIN_SCALE) / (1 - MIN_SCALE)) * (1 - MIN_ALPHA)))
+                }
+                else -> {
+                    alpha = 0f
+                }
+            }
+        }
+    }
 }
